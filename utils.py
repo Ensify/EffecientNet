f9d17a06cdb2cc09191943c3c7bf59651ce4f9b7
Ensify <vignesh1234can@gmail.com>
2024-07-20 22:54:10 +0530

    first commit

---
+++ utils.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+def plot_metrics(history):
+  metrics = ['loss', 'prc', 'precision', 'recall']
+  for n, metric in enumerate(metrics):
+    name = metric.replace("_"," ").capitalize()
+    plt.subplot(2,2,n+1)
+    plt.plot(history.epoch, history.history[metric], label='Train')
+    plt.plot(history.epoch, history.history['val_'+metric]
+          , linestyle="--", label='Val')
+    plt.xlabel('Epoch')
+    plt.ylabel(name)
+    if metric == 'loss':
+      plt.ylim([0, plt.ylim()[1]])
+    elif metric == 'auc':
+      plt.ylim([0.8,1])
+    else:
+      plt.ylim([0,1])
+
+    plt.legend()
+    
+  plt.savefig('model_metrics.png')
